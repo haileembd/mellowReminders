6017cefe6331c4ccf7dad67f7a6b48c166e438a8
Hailee Davis <haileembd@gmail.com>
2021-07-22 14:58:37 -0500

    Initial Commit

---
+++ mellowReminders/AddViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddViewController.swift
+//  mellowReminders
+//
+//  Created by  Scholar on 7/22/21.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet var titleField: UITextField!
+    @IBOutlet var bodyField: UITextField!
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    public var completion: ((String, String, Date) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func didTapSaveButton() {
+        if let titleText = titleField.text, !titleText.isEmpty,
+           let bodyText = bodyField.text, !bodyText.isEmpty {
+          
+           let targetDate = datePicker.date
+        }
+    }
+
+}
---
+++ mellowReminders/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  mellowReminders
+//
+//  Created by  Scholar on 7/22/21.
+//
+import UserNotifications
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var table: UITableView!
+    
+    var models = [MyReminder]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTappAdd() {
+        //show add view controller
+    }
+    
+    @IBAction func didTapTest() {
+        //fire test notification
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { success, error in
+            if success {
+                //schdeule test
+                self.scheduleTest()
+            }
+            else if let error = error {
+                print("error occured")
+            }
+        })
+    }
+    
+    func scheduleTest() {
+        let content = UNMutableNotificationContent()
+        content.title = "Hello World"
+        content.sound = .default
+        content.body = "hey this is a notif"
+        
+        let targetDate = Date().addingTimeInterval(10)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second],
+                                                                                                  from: targetDate), repeats: true)
+        
+        let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            if error != nil {
+                print("something went wrong")
+            }
+        })
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        
+        return cell
+    }
+}
+
+struct MyReminder {
+    let title: String
+    let date: Date
+    let identifier: String
+}
