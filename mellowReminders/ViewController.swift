760ff1faec64568a7d0599c40660ae385f3fb318
ImanSuleman <56176914+ImanSuleman@users.noreply.github.com>
2021-07-23 01:16:35 -0500

    done

--- mellowReminders/ViewController.swift
+++ mellowReminders/ViewController.swift
@@ -10,19 +10,49 @@ import UIKit
 //again
 class ViewController: UIViewController {
 
+    
+
     @IBOutlet var table: UITableView!
     
     var models = [MyReminder]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         table.delegate = self
         table.dataSource = self
-        // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func didTappAdd() {
         //show add view controller
+        guard let vc = storyboard?.instantiateViewController(identifier: "add") as? AddViewController else {
+            return
+        }
+        vc.title = "New Reminder"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.completion = { title, body, date in
+        DispatchQueue.main.async {
+                self.navigationController?.popToRootViewController(animated: true)
+                let new = MyReminder(title: title, date: date, identifier: "id_\(title)")
+                self.models.append(new)
+                self.table.reloadData()
+                
+                let content = UNMutableNotificationContent()
+                content.title = title
+                content.sound = .default
+                content.body = body
+                
+                let targetDate = date
+                let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second],
+                                                                                                          from: targetDate), repeats: false)
+                
+                let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+                    if error != nil {
+                        print("something went wrong")
+                    }
+                })
+            }
+        }
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func didTapTest() {
@@ -32,7 +62,7 @@ class ViewController: UIViewController {
                 //schdeule test
                 self.scheduleTest()
             }
-            else if let error = error {
+            else if error != nil {
                 print("error occured")
             }
         })
@@ -46,8 +76,7 @@ class ViewController: UIViewController {
         
         let targetDate = Date().addingTimeInterval(10)
         let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second],
-                                                                                                  from: targetDate), repeats: true)
-        
+                                                                                                  from: targetDate), repeats: false)
         let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
         UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
             if error != nil {
@@ -55,18 +84,21 @@ class ViewController: UIViewController {
             }
         })
     }
-    
 }
 
 extension ViewController: UITableViewDelegate {
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
 extension ViewController: UITableViewDataSource {
-    
+    func UITableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -78,7 +110,11 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = models[indexPath.row].title
+        let date = models[indexPath.row].date
         
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/YYYY at hh:mm a"
+        cell.detailTextLabel?.text = formatter.string(from: date)
         return cell
     }
 }
